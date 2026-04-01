409582cd1d6b710909fe910f8452b92e8db1ef88
protapchakma <protapchakma43@gmail.com>
2026-04-01 16:42:29 +0600

    status update

--- src/components/StatusSection/StatusSection.jsx
+++ src/components/StatusSection/StatusSection.jsx
@@ -2,21 +2,25 @@ import React from 'react';
 
 const StatusSection = () => {
     return (
-        <div className='bg-linear-to-r from-blue-500 to-purple-500'>
+        <div className='bg-linear-to-r from-blue-300 to-purple-300'>
             <div className='flex max-sm:flex-col justify-between items-center text-center py-13 w-8/12 mx-auto text-white'>
+
                 <div>
-                    <h1 className='text-5xl font-bold'>50k+</h1>
-                    <p className='opacity-90'>Active Users</p>
+                    <h1 className='text-6xl font-bold'>50k+</h1>
+                    <p className='opacity-80'>Active Users</p>
                 </div>
+
                 <div className="divider sm:divider-horizontal before:bg-gray-300 after:bg-gray-300"></div>
                 <div>
-                    <h1 className='text-5xl font-bold'>200+</h1>
-                    <p className='opacity-90'>Premium tools</p>
+                    <h1 className='text-6xl font-bold'>200+</h1>
+                    <p className='opacity-80'>Premium tools</p>
                 </div>
+
+                
                 <div className="divider sm:divider-horizontal before:bg-gray-300 after:bg-gray-300"></div>
                 <div>
-                    <h1 className='text-5xl font-bold'>4.9</h1>
-                    <p className='opacity-90'>Rating</p>
+                    <h1 className='text-6xl font-bold'>4.9</h1>
+                    <p className='opacity-80'>Rating</p>
 
                 </div>
 
